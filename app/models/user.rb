f87fd4183d150c264a5d2ac435bd4cb7064e70e4
brainycodings <brainycodings@gmail.com>
2025-07-16 16:29:49 +0200

    controller user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
 
   has_one :city, dependent: :destroy
   has_one :architect, dependent: :destroy
-
+  has_one_attached :avatar
+  
   enum :role, {
     admin: 0,
     client: 1,
