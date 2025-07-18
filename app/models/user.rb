cb60a45672857948e294c71166c24407a75f47fc
RosaBen <benchabane.rosa@outlook.com>
2025-07-18 10:04:17 +0200

    removed validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,6 @@ class User < ApplicationRecord
     architect: 2
   }
 
-  validates :role, presence: true
-  validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
 
   # Validation conditionnelle : les architectes doivent avoir une ville
