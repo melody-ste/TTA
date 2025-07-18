5791c007f592d0130ca311440c6870703b4b35ea
melody-ste <melody_stephan@icloud.com>
2025-07-18 15:31:03 +0200

    remove the portfolio migration

--- db/migrate/20250718131850_drop_portfolios.rb
+++ db/migrate/20250718131850_drop_portfolios.rb
@@ -1,6 +1,6 @@
-class CreatePortfolios < ActiveRecord::Migration[8.0]
+class DropPortfolios < ActiveRecord::Migration[8.0]
   def change
-    create_table :portfolios do |t|
+    drop_table :portfolios do |t|
       t.references :architect, null: false, foreign_key: true
       t.string :project_title
       t.text :project_description
