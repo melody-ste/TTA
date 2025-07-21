12757b5e5a65a9a1517e9ec3b101ebbf357bfaf5
RosaBen <benchabane.rosa@outlook.com>
2025-07-21 20:27:03 +0200

    added translation with fr.yml

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Tta
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [ :fr, :en ]
   end
 end
