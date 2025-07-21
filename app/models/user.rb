2aad5d17f6356a63412ff239d1ff27630e7825d6
RosaBen <benchabane.rosa@outlook.com>
2025-07-21 13:44:19 +0200

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :trackable, :validatable
+         :confirmable
+  
   has_one :city, dependent: :destroy
   has_one :architect, dependent: :destroy
   has_many :projects, dependent: :destroy
